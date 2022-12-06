9a8722dd5aaeed12277b48a36cef4cbb730a2e49
Abolfazl <abolfazljamshididev@gmail.com>
2022-12-06 16:06:30 +0330

    add product to shopping cart feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import routes from "./Routes/routes";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-    <Routes>
-      {routes && routes.map((route) => <Route {...route} key={route.id} />)}
-    </Routes>
+    <Provider store={store}>
+      <Routes>
+        {routes && routes.map((route) => <Route {...route} key={route.id} />)}
+      </Routes>
+    </Provider>
   );
 }
 
